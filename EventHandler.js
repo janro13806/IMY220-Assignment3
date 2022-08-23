eb5b77cb9a2546f240ce5cd464c3610488c1ba14
janro13806 <botha.janro@gmail.com>
2022-08-23 20:39:08 +0200

    final commits

--- EventHandler.js
+++ EventHandler.js
@@ -1,6 +1,7 @@
 function EventHandler(objArr){
     this.objArr = objArr;
 
+
     this.getEventsBetweenDates = function(start, end){
         return  this.objArr.filter(dateHelp);
     
@@ -31,30 +32,65 @@ function EventHandler(objArr){
     }
 
     this.getSummary = function(){
-        StringArr = Array.prototype.slice.apply(arguments)[0];
+        let StringArr;
+
+        if (arguments.length == 0) {
+            StringArr = this.objArr;
+        } else {
+            let janA = Array.prototype.slice.apply(arguments)[0];
+
+            if (janA.constructor === Array) {
+                StringArr = janA;  
+            } else {
+                StringArr = Array.prototype.slice.apply(arguments);
+            }
+        } 
         var arrReturn = [];
 
-         StringArr.map(function(x) {
+        StringArr.map(function(x) {
             if (x.dateStart == x.dateEnd) {
-               arrReturn.push("On " + x.dateStart + ":"
-               + x.name + "(" + x.description + ")");          
+            arrReturn.push("On " + x.dateStart + ":"
+            + x.name + "(" + x.description + ")");          
             } else {
-                arrReturn.push("From " + x.dateStart + " to "
-                + x.dateEnd + ": "
-                + x.name + "(" + x.description + ")");          
+            arrReturn.push("From " + x.dateStart + " to "
+            + x.dateEnd + ": "
+            + x.name + "(" + x.description + ")");          
             }
         });
-        return arrReturn;
+        return arrReturn;  
     }
+
+    // Helper prototype 1: 
+    Array.prototype.getEventsBetweenDates = function(start, end){
+        return new EventHandler(this).getEventsBetweenDates(start, end);
+    }
+
+    // Helper prototype 2: 
+    Array.prototype.getByMonth = function(month){
+        return new EventHandler(this).getByMonth(month);
+    }
+
+    // Helper prototype 3: 
+    Array.prototype.getUniqueDateAndSort = function(){
+        return new EventHandler(this).getUniqueDateAndSort();
+    }
+
+    // Helper prototype 4: 
+    Array.prototype.getSummary = function(){
+        return new EventHandler(this).getSummary(this);
+    }
+
 
 }
 
 var EH1 = new EventHandler(events);
-console.log("TESTING : ");
-console.log(EH1.getEventsBetweenDates("2022/02/01", "2022/02/16"));
-console.log("_________________________________________");
-console.log(EH1.getByMonth(05));
-console.log("_________________________________________");
-console.log(EH1.getUniqueDateAndSort());
-console.log("_________________________________________");
-console.log(EH1.getSummary(events));
+// console.log("TESTING : ");
+// console.log(EH1.getEventsBetweenDates("2022/02/01", "2022/02/16"));
+// console.log("_________________________________________");
+// console.log(EH1.getByMonth(05));
+// console.log("_________________________________________");
+// console.log(EH1.getUniqueDateAndSort());
+// console.log("_________________________________________");
+// console.log(EH1.getSummary());
+// console.log("_________________________________________");
+// console.log(EH1.getByMonth(06).getSummary());
